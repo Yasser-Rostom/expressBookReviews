37983d7a16d964bbdf7c7dcf0a4f2891317cdd30
Yasser Rostom <yasser9340@gmail.com>
2024-06-06 08:16:52 -0400

    task 3 and 4

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -32,15 +32,49 @@ public_users.get('/isbn/:isbn',function (req, res) {
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let booksFound = [];
+    
+    if (author) {
+        for (let key in books) {
+            const book = books[key];
+            if (book.author === author) {
+                booksFound.push(book);
+            }
+        }
+
+        if (booksFound.length > 0) {
+            return res.json(booksFound);
+        } else {
+            return res.status(404).json({ message: "No books for the author found" });
+        }
+    }
+    
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const title = req.params.title;
+    let booksFound = [];
+    
+    if (title) {
+        for (let key in books) {
+            const book = books[key];
+            if (book.title === title) {
+                booksFound.push(book);
+            }
+        }
+
+        if (booksFound.length > 0) {
+            return res.json(booksFound);
+        } else {
+            return res.status(404).json({ message: "No books for the author found" });
+        }
+    }
+    
+    return res.status(300).json({ message: "Yet to be implemented" });
 });
 
 //  Get book review
